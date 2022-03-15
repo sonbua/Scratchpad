5b7edc5426c04718dc4948b59e8e0e55016d4463
Son Nguyen <son.nguyen@optimizely.com>
2022-03-15 18:58:20 +0700

    Benchmark `foreach` loop on T[], List<T>, and IEnumerable<T>

---
+++ test/Scratchpad.Tests/ForeachBenchmarkTest.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Xunit;
+
+namespace Scratchpad.Tests;
+
+public class ForeachBenchmarkTest
+{
+    [Fact]
+    public void RunBenchmark()
+    {
+        BenchmarkRunner.Run<ForeachOnArrayVsListVsEnumerable>();
+    }
+
+    [MemoryDiagnoser]
+    public class ForeachOnArrayVsListVsEnumerable
+    {
+        private readonly int[] _array;
+        private readonly List<int> _list;
+        private readonly IEnumerable<int> _enumerable;
+
+        public ForeachOnArrayVsListVsEnumerable()
+        {
+            _array = Enumerable.Range(1, 10).ToArray();
+            _list = _array.ToList();
+            _enumerable = _list;
+        }
+
+        [Benchmark]
+        public void ForeachOnArray()
+        {
+            foreach (var item in _array)
+            {
+            }
+        }
+
+        [Benchmark]
+        public void ForeachOnList()
+        {
+            foreach (var item in _list)
+            {
+            }
+        }
+
+        [Benchmark]
+        public void ForeachOnEnumerable()
+        {
+            foreach (var item in _enumerable)
+            {
+            }
+        }
+    }
+}
