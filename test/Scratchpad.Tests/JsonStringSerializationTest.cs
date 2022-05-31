77ec90fd47f77c34ee029b51ffed80e9b2847fb9
Son Nguyen <son.nguyen@optimizely.com>
2022-05-31 18:13:48 +0700

    Add JSON serialization tests for special characters
    References
    - https://www.ecma-international.org/publications-and-standards/standards/ecma-404/
    - https://www.json.org/json-en.html

---
+++ test/Scratchpad.Tests/JsonStringSerializationTest.cs
@@ -0,0 +1,57 @@
+using Newtonsoft.Json;
+using Xunit;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace Scratchpad.Tests;
+
+public class JsonStringSerializationTest
+{
+    [Theory(Skip = "some tests fail")]
+    [MemberData(nameof(InputAndLengthAndSerializedValue))]
+    public void Newtonsoft_SpecialCharacter_ShouldEscape(
+        string input,
+        int inputLength,
+        string expectedSerializedValue)
+    {
+        Assert.Equal(inputLength, input.Length);
+
+        var actual = JsonConvert.SerializeObject(input);
+
+        Assert.Equal($"\"{expectedSerializedValue}\"", actual);
+
+        var deserialized = JsonConvert.DeserializeObject($"\"{expectedSerializedValue}\"");
+
+        Assert.Equal(input, deserialized);
+    }
+
+    [Theory(Skip = "some tests fail")]
+    [MemberData(nameof(InputAndLengthAndSerializedValue))]
+    public void SystemText_SpecialCharacter_ShouldEscape(
+        string input,
+        int inputLength,
+        string expectedSerializedValue)
+    {
+        Assert.Equal(inputLength, input.Length);
+
+        var actual = JsonSerializer.Serialize(input);
+
+        Assert.Equal($"\"{expectedSerializedValue}\"", actual);
+
+        var deserialized = JsonConvert.DeserializeObject($"\"{expectedSerializedValue}\"");
+
+        Assert.Equal(input, deserialized);
+    }
+
+    public static TheoryData<string, int, string> InputAndLengthAndSerializedValue = new()
+    {
+        // input, length, serialized value
+        { "\"", 1, @"\""" },
+        { @"\", 1, @"\\" },
+        { @"/", 1, @"\/" },
+        { "\b", 1, @"\b" },
+        { "\f", 1, @"\f" },
+        { "\n", 1, @"\n" },
+        { "\r", 1, @"\r" },
+        { "\t", 1, @"\t" },
+    };
+}
