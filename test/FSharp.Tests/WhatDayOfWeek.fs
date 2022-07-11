44c6d70f08adf158766dd3c7f2062ba9a2ecaa32
Son Nguyen <son.nguyen@optimizely.com>
2022-07-11 08:54:30 +0700

    Specify what day of week for a given year/month/day
    Also update dependencies

---
+++ test/FSharp.Tests/WhatDayOfWeek.fs
@@ -0,0 +1,85 @@
+module FSharp.Tests.WhatDayOfWeek
+
+open System
+open FsUnit
+open Xunit
+
+let countDaysUpToYear year =
+    let year = year - 1
+    let century = year / 100
+
+    year * 365 + year / 4 - century + century / 4
+
+let leapYearMonthToDayMap =
+    [ 0
+      31
+      29
+      31
+      30
+      31
+      30
+      31
+      31
+      30
+      31
+      30
+      31 ]
+
+let commonYearMonthToDayMap =
+    [ 0
+      31
+      28
+      31
+      30
+      31
+      30
+      31
+      31
+      30
+      31
+      30
+      31 ]
+
+let isLeapYear year =
+    let mod' x y = y % x = 0
+
+    match year with
+    | _ when (mod' 400 year) -> true
+    | _ when mod' 100 year -> false
+    | _ when mod' 4 year -> true
+    | _ -> false
+
+let (|LeapYear|CommonYear|) year =
+    let mod' x y = y % x = 0
+
+    match year with
+    | _ when (mod' 400 year) -> LeapYear
+    | _ when mod' 100 year -> CommonYear
+    | _ when mod' 4 year -> LeapYear
+    | _ -> CommonYear
+
+let daysInMonth year month =
+    match year with
+    | LeapYear -> leapYearMonthToDayMap
+    | CommonYear -> commonYearMonthToDayMap
+    |> List.item month
+
+let countDaysInYearUpToMonth year month =
+    [ 1 .. (month - 1) ]
+    |> List.map (daysInMonth year)
+    |> List.sum
+
+let countDays (year, month, day) =
+    countDaysUpToYear year
+    + countDaysInYearUpToMonth year month
+    + day
+
+let whatDayOfWeek =
+    countDays >> (fun x -> x % 7) >> enum<DayOfWeek>
+
+[<Theory>]
+[<InlineData(2022, 7, 5, DayOfWeek.Tuesday)>]
+[<InlineData(2022, 1, 1, DayOfWeek.Saturday)>]
+let ``Given year month day Should return correct day of week`` year month day (expected: DayOfWeek) =
+    whatDayOfWeek (year, month, day)
+    |> should equal expected
