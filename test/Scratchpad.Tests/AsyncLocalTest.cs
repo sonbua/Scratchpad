35544ce00fff4cfd03d60987ad5759af06f68966
Son Nguyen <son.nguyen@optimizely.com>
2022-08-29 15:41:57 +0700

    Add AsyncLocal test

---
+++ test/Scratchpad.Tests/AsyncLocalTest.cs
@@ -0,0 +1,36 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Scratchpad.Tests;
+
+public class AsyncLocalTest
+{
+    [Fact]
+    public void ShouldFlowAcrossTaskRun()
+    {
+        Accessor.Context.Value = "hello";
+
+        Task.Run(() => Assert.Equal("hello", Accessor.Context.Value)).Wait();
+    }
+
+    [Fact]
+    public void ShouldBeRestoredEvenIfBeingTampered()
+    {
+        Accessor.Context.Value = "hello";
+
+        Task.Run(() =>
+        {
+            Assert.Equal("hello", Accessor.Context.Value);
+
+            Accessor.Context.Value = "tampered";
+        }).Wait();
+
+        Assert.Equal("hello", Accessor.Context.Value);
+    }
+
+    static class Accessor
+    {
+        public static readonly AsyncLocal<string> Context = new AsyncLocal<string>();
+    }
+}
