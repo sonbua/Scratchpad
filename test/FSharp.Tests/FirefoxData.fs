f4e952d44b718f38c4ef3ea9726206a628b1dc90
Son Nguyen <son.nguyen@optimizely.com>
2023-06-15 13:26:26 +0700

    fix

--- test/FSharp.Tests/FirefoxData.fs
+++ test/FSharp.Tests/FirefoxData.fs
@@ -3,7 +3,7 @@ namespace Firefox
 open System
 open System.IO
 open System.Text.RegularExpressions
-
+aoeu
 module Data =
     let path =
         "C:\\Users\\song\\AppData\\Roaming\\librewolf\\Profiles\\5d5x89ro.default-default\\storage\\default\\"
