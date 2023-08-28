51529a109339cce4d87e639c79e6840b2d97b90b
Son Nguyen <son.nguyen@optimizely.com>
2023-08-28 12:48:46 +0700

    app.UseRouting() is redundant
    WebApplicationBuilder configures a middleware pipeline that wraps
    middleware added in Program.cs with UseRouting and UseEndpoints.
    Unless we want to change the order in which UseRouting and
    UseEndpoints run, they can be removed.

--- src/WebApplication/Program.cs
+++ src/WebApplication/Program.cs
@@ -20,8 +20,6 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 
-app.UseRouting();
-
 app.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
 app.MapControllers();
 
