7a5514fd8fe489ab36c74dd654f7a862ececbe64
Son Nguyen <dinh.son.nguyen@gmail.com>
2024-08-21 13:31:09 +0700

    Migrate Logseq.fs tests to Expecto
    Migrate to Expecto test library in FSharp.Tests project, part 7.

--- test/FSharp.Tests/Logseq.fs
+++ test/FSharp.Tests/Logseq.fs
@@ -12,18 +12,15 @@ type BackupDirectory = {
 }
 
 module BackupDirectory =
-    /// Lists all the backup items within the backup directory.
+    /// Lists all the backup items within a backup directory.
     let items (dir: BackupDirectory): FileInfo list =
         dir.Directory |> DirectoryInfo.getMatchingFiles dir.FilePattern |> toList
 
     /// Deletes all backup items except the most recent ones.
     /// The number of skipped items is provided by <paramref name="options"/>.
-    /// <param name="options"></param>
     let cleanup options (dir: BackupDirectory): string list =
         let deleting = dir |> items |> rev |> List.skipSafe options.ItemsToKeep |> map _.FullName
-
         deleting |> map File.delete |> ignore
-
         deleting
 
 type RootBackupDirectory = {
@@ -32,7 +29,7 @@ type RootBackupDirectory = {
 }
 
 module RootBackupDirectory =
-    let private moreThan n = (<) n
+    let inline private moreThan n = (<) n
 
     let pendingCleanupFolders options rootDir: BackupDirectory list =
         rootDir.RootDirectory
@@ -44,15 +41,24 @@ module RootBackupDirectory =
     let cleanup options rootDir =
         rootDir |> pendingCleanupFolders options |> map (BackupDirectory.cleanup options)
 
-let rootBackupDirectories =
-    [
-        { RootDirectory = DirectoryInfo @"C:\Users\song\OneDrive - Episerver\doc\Note\logseq\bak\pages\"; FilePattern = "*.md" }
-        { RootDirectory = DirectoryInfo @"C:\Users\song\OneDrive - Episerver\doc\Note\logseq\bak\journals\"; FilePattern = "*.md" }
-        { RootDirectory = DirectoryInfo @"C:\Users\song\OneDrive - Episerver\doc\Note\logseq\bak\logseq\"; FilePattern = "*.edn" }
-        { RootDirectory = DirectoryInfo @"C:\Users\song\OneDrive - Episerver\doc\Note\logseq\bak\logseq\"; FilePattern = "*.css" }
-    ]
-let options = { ItemsToKeep = 1 }
 
-// Usage
-// rootBackupDirectories |> map (RootBackupDirectory.pendingCleanupFolders options)
-// rootBackupDirectories |> map (RootBackupDirectory.cleanup options)
+open Expecto
+open Expecto.Logging
+
+[<Tests>]
+let specs =
+    test "Logseq" {
+        let rootBackupDirectories =
+            [ { RootDirectory = DirectoryInfo @"C:\Users\song\OneDrive - Episerver\doc\Note\logseq\bak\pages\"; FilePattern = "*.md" }
+              { RootDirectory = DirectoryInfo @"C:\Users\song\OneDrive - Episerver\doc\Note\logseq\bak\journals\"; FilePattern = "*.md" }
+              { RootDirectory = DirectoryInfo @"C:\Users\song\OneDrive - Episerver\doc\Note\logseq\bak\logseq\"; FilePattern = "*.edn" }
+              { RootDirectory = DirectoryInfo @"C:\Users\song\OneDrive - Episerver\doc\Note\logseq\bak\logseq\"; FilePattern = "*.css" } ]
+        let options = { ItemsToKeep = 1 }
+        let logger = Log.create "Logseq"
+        let writeln = Message.eventX >> logger.info
+
+        rootBackupDirectories
+        |> map (RootBackupDirectory.pendingCleanupFolders options)
+        // |> map (RootBackupDirectory.cleanup options)
+        |> (sprintf "%A" >> writeln)
+    }
