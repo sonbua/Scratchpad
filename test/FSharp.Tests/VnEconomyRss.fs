a9190a58c3bcc0381517e9a8fe2c3ea4cf8fcfd2
Son Nguyen <dinh.son.nguyen@gmail.com>
2024-08-21 13:32:15 +0700

    Migrate VnEconomyRss.fs tests to Expecto
    Migrate to Expecto test library in FSharp.Tests project, part 8.

--- test/FSharp.Tests/VnEconomyRss.fs
+++ test/FSharp.Tests/VnEconomyRss.fs
@@ -3,8 +3,6 @@ module VnEconomyRss
 open System.Text.RegularExpressions
 open FSharpPlus
 open FSharp.Data
-open Xunit
-open Xunit.Abstractions
 
 [<Literal>]
 let RssFeedUrl = "https://vneconomy.vn/chung-khoan.rss"
@@ -22,18 +20,28 @@ module RssFeedItem =
           Published = item.PubDate.UtcDateTime |> stringf "dd/MM/yyyy"
           Link = item.Link }
 
-type Test(helper: ITestOutputHelper) =
+
+open Expecto
+open Expecto.Logging
+
+[<Tests>]
+let specs =
+    /// Was used to decode the description
     let charDecode input =
         let charCodeToString (m: Match) =
             m.Groups[1].Value |> int |> char |> string
 
         Regex.Replace(input, "\#(\d+);", charCodeToString)
 
-    [<Fact>]
-    member _.``Blog chứng khoán``() =
-        RssFeed.GetSample()
-        |> _.Channel.Items
-        |> filter (fun x -> x.Title.Contains "Blog chứng khoán")
-        |> sortByDescending _.PubDate
-        |> map RssFeedItem.toFeedItem
-        |> iter (sprintf "%A" >> helper.WriteLine)
+    testList "VnEconomyRss" [
+        let logger = Log.create "VnEconomyRss"
+        let writeln = Message.eventX >> logger.info
+
+        test "Blog chứng khoán" {
+            RssFeed.GetSample()
+            |> _.Channel.Items
+            |> filter (fun x -> x.Title.Contains "Blog chứng khoán")
+            |> sortByDescending _.PubDate
+            |> map RssFeedItem.toFeedItem
+            |> (sprintf "%A" >> writeln)
+        } ]
