577b3ff3483e012aadf679c1df3082c5e6300f82
Son Nguyen <dinh.son.nguyen@gmail.com>
2024-11-15 23:06:43 +0700

    Add script to clean up Rider log folder

---
+++ test/FSharp.Tests/RiderLog.fs
@@ -0,0 +1,101 @@
+module RiderLog
+
+// #r "nuget: Fake.Core.Target"
+// #r "nuget: FSharpPlus"
+
+open System
+open System.IO
+open FSharpPlus
+open Fake.IO
+
+module File =
+    let tryDelete path : Result<string, exn> =
+        try
+            File.delete path
+            Ok path
+        with exn ->
+            Error exn
+
+    let tryDeleteAll paths : Result<string, exn> list = paths |> List.map tryDelete
+
+type CleanupOptions =
+    { LogRootDir: string
+      CutOffTimestamp: DateTime }
+
+let private beforeCutOffTimestamp timestamp arg : bool = arg < timestamp
+
+let private tryFindSubDirByName dirName parentDir : DirectoryInfo option =
+    parentDir
+    |> DirectoryInfo.getSubDirectories
+    |> tryFind (fun dir -> dir.Name = dirName)
+
+let private cleanupDirs beforeCutOffTimestamp rootDir : string list =
+    let deletingThreadDumps rootDir =
+        rootDir
+        |> DirectoryInfo.getSubDirectories
+        |> filter (_.Name >> String.startsWith "threadDumps-freeze-")
+        |> toList
+
+    let deletingDebuggerWorker rootDir =
+        rootDir
+        |> tryFindSubDirByName "DebuggerWorker"
+        |> Option.map DirectoryInfo.getSubDirectories
+        |> Option.defaultValue [||]
+        |> toList
+
+    let deletingDirs =
+        [ rootDir ]
+        |> List.apply [ deletingThreadDumps; deletingDebuggerWorker ]
+        |> List.concat
+        |> filter (_.LastWriteTime >> beforeCutOffTimestamp)
+        |> map _.FullName
+
+    deletingDirs |> map Directory.delete |> ignore
+
+    deletingDirs
+
+let private cleanupFiles beforeCutOffTimestamp rootDir : string list =
+    let dirsToCleanup rootDir =
+        [ rootDir ]
+        |> List.apply
+            [ Some
+              tryFindSubDirByName "SolutionBuilder"
+              tryFindSubDirByName "BackendThreadDump" ]
+        |> choose id
+
+    rootDir
+    |> dirsToCleanup
+    |> List.collect (DirectoryInfo.getFiles >> toList)
+    |> filter (_.LastWriteTime >> beforeCutOffTimestamp)
+    |> map _.FullName
+    |> File.tryDeleteAll
+    |> choose Result.toOption
+
+let cleanup options : string list =
+    let logRootDir =
+        let dir = options.LogRootDir |> DirectoryInfo.ofPath
+        dir |> DirectoryInfo.ensure
+        dir
+
+    let beforeCutOffTimestamp' = options.CutOffTimestamp |> beforeCutOffTimestamp
+    let cleanupDirs' = cleanupDirs beforeCutOffTimestamp'
+    let cleanupFiles' = cleanupFiles beforeCutOffTimestamp'
+
+    [ logRootDir ] |> List.apply [ cleanupDirs'; cleanupFiles' ] |> List.concat
+
+
+open Expecto
+open Expecto.Logging
+
+[<Tests>]
+let specs =
+    let logger = Log.create "RiderLog"
+    let writeln = Message.eventX >> logger.info
+
+    ptest "Run cleanup" {
+        let options =
+            { LogRootDir = "R:\\idea-log\\"
+              CutOffTimestamp = DateTime.Now.Date }
+
+        cleanup options |> map writeln |> ignore
+    }
