8a8715dd630809103a4078ea6c1d7f8b07a8d030
Son Nguyen <dinh.son.nguyen@gmail.com>
2024-12-28 15:32:10 +0700

    Refactor Logseq.fs

--- test/FSharp.Tests/Logseq.fs
+++ test/FSharp.Tests/Logseq.fs
@@ -6,40 +6,64 @@ open FSharpPlus
 
 type Options = { ItemsToKeep: int }
 
-type BackupDirectory = {
-    Directory: DirectoryInfo
-    FilePattern: string
-}
+type BackupDirectory =
+    { Directory: DirectoryInfo
+      FilePattern: string }
 
 module BackupDirectory =
+    let create filePattern dir : BackupDirectory =
+        { Directory = dir
+          FilePattern = filePattern }
+
     /// Lists all the backup items within a backup directory.
-    let items (dir: BackupDirectory): FileInfo list =
+    let items dir : FileInfo list =
         dir.Directory |> DirectoryInfo.getMatchingFiles dir.FilePattern |> toList
 
     /// Deletes all backup items except the most recent ones.
-    /// The number of skipped items is provided by <paramref name="options"/>.
-    let cleanup options (dir: BackupDirectory): string list =
-        let deleting = dir |> items |> rev |> List.skipSafe options.ItemsToKeep |> map _.FullName
+    let cleanup itemsToKeep dir : string list =
+        let deleting = dir |> items |> rev |> List.skipSafe itemsToKeep |> map _.FullName
+
         deleting |> map File.delete |> ignore
         deleting
 
-type RootBackupDirectory = {
-    RootDirectory: DirectoryInfo
-    FilePattern: string
-}
+type PendingCleanupDirectory =
+    | HasPendingItems of BackupDirectory * itemsToKeep: int
+    | Empty of DirectoryInfo
 
-module RootBackupDirectory =
+module PendingCleanupDirectory =
     let inline private moreThan n = (<) n
 
-    let pendingCleanupFolders options rootDir: BackupDirectory list =
+    let create options dir : PendingCleanupDirectory option =
+        if dir.Directory |> DirectoryInfo.getFiles |> Array.isEmpty then
+            dir.Directory |> Empty |> Some
+        elif dir |> BackupDirectory.items |> length |> moreThan options.ItemsToKeep then
+            (dir, options.ItemsToKeep) |> HasPendingItems |> Some
+        else
+            None
+
+    let cleanup dir : string list =
+        match dir with
+        | HasPendingItems(d, itemsToKeep) -> BackupDirectory.cleanup itemsToKeep d
+        | Empty d ->
+            d.Delete()
+            [ d.FullName ]
+
+type RootBackupDirectory =
+    { RootDirectory: DirectoryInfo
+      FilePattern: string }
+
+module RootBackupDirectory =
+    let pendingCleanupDirectories options rootDir : PendingCleanupDirectory list =
         rootDir.RootDirectory
         |> DirectoryInfo.getSubDirectories
         |> toList
-        |> map (fun x -> { Directory = x; FilePattern = rootDir.FilePattern })
-        |> filter (BackupDirectory.items >> length >> moreThan options.ItemsToKeep)
+        |> map (BackupDirectory.create rootDir.FilePattern)
+        |> choose (PendingCleanupDirectory.create options)
 
-    let cleanup options rootDir =
-        rootDir |> pendingCleanupFolders options |> map (BackupDirectory.cleanup options)
+    let cleanup options rootDir : string list list =
+        rootDir
+        |> pendingCleanupDirectories options
+        |> map PendingCleanupDirectory.cleanup
 
 
 open Expecto
@@ -53,12 +77,13 @@ let specs =
               { RootDirectory = DirectoryInfo @"C:\Users\song\OneDrive - Episerver\doc\Note\logseq\bak\journals\"; FilePattern = "*.md" }
               { RootDirectory = DirectoryInfo @"C:\Users\song\OneDrive - Episerver\doc\Note\logseq\bak\logseq\"; FilePattern = "*.edn" }
               { RootDirectory = DirectoryInfo @"C:\Users\song\OneDrive - Episerver\doc\Note\logseq\bak\logseq\"; FilePattern = "*.css" } ]
+
         let options = { ItemsToKeep = 1 }
         let logger = Log.create "Logseq"
         let writeln = Message.eventX >> logger.info
 
         rootBackupDirectories
-        |> map (RootBackupDirectory.pendingCleanupFolders options)
+        |> map (RootBackupDirectory.pendingCleanupDirectories options)
         // |> map (RootBackupDirectory.cleanup options)
         |> (sprintf "%A" >> writeln)
     }
