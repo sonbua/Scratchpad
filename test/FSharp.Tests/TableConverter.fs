0741e1483d1ca0e1287703a29fc8a8226cfeef85
Son Nguyen <dinh.son.nguyen@gmail.com>
2025-08-22 00:11:12 +0700

    Add Markdown to tab conversion

--- test/FSharp.Tests/TableConverter.fs
+++ test/FSharp.Tests/TableConverter.fs
@@ -6,7 +6,7 @@ module TableConverter
 open System
 open FSharpPlus
 
-module Regex =
+module internal Regex =
     open System.Text.RegularExpressions
 
     let replace pattern (replacement: string) text : string =
@@ -14,12 +14,23 @@ module Regex =
 
 module internal Line =
     let tabToMarkdown line : string =
-        line |> Regex.replace "\\t" " | " |> sprintf "| %s |"
+        line |> String.replace "\t" " | " |> sprintf "| %s |"
+
+    let markdownToTab (line: string) : string =
+        line.Split("|", StringSplitOptions.RemoveEmptyEntries)
+        |> map String.trimWhiteSpaces
+        |> String.concat "\t"
 
 let private newLines = [| "\r\n"; "\n"; Environment.NewLine |]
 
 let tabularToMarkdown (text: string) : string =
     text.Split(newLines, StringSplitOptions.RemoveEmptyEntries)
-    |> map String.trimWhiteSpaces
+    |> map (String.trim [ ' '  ])
     |> map Line.tabToMarkdown
+    |> fun lines -> String.Join(Environment.NewLine, lines)
+
+let markdownToTabular (text: string) : string =
+    text.Split(newLines, StringSplitOptions.RemoveEmptyEntries)
+    |> map String.trimWhiteSpaces
+    |> map Line.markdownToTab
     |> fun lines -> String.Join(Environment.NewLine, lines)
